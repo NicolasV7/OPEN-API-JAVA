d362da0b04bc50485ff23965ebacf47807784728
NicolasV7 <159790870+NicolasV7@users.noreply.github.com>
2024-03-12 23:49:00 -0500

    Add files via upload

---
+++ resolve-enigma-api-step-1/src/main/java/co/com/villo/microservice/resolveEnigmaApi/api/GetStepApiController.java
@@ -0,0 +1,85 @@
+package co.com.villo.microservice.resolveEnigmaApi.api;
+
+import co.com.villo.microservice.resolveEnigmaApi.model.ErrorDetail;
+import co.com.villo.microservice.resolveEnigmaApi.model.GetEnigmaStepResponse;
+import co.com.villo.microservice.resolveEnigmaApi.model.JsonApiBodyRequest;
+import co.com.villo.microservice.resolveEnigmaApi.model.JsonApiBodyResponseSuccess;
+import co.com.villo.microservice.resolveEnigmaApi.model.JsonApiBodyResponseErrors;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.annotations.ApiParam;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
+import javax.servlet.http.HttpServletRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2024-02-28T01:53:31.617-05:00[America/Bogota]")
+@Controller
+public class GetStepApiController implements GetStepApi {
+	private static final Logger log = LoggerFactory.getLogger(GetStepApiController.class);
+    private final ObjectMapper objectMapper;
+    private final HttpServletRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public GetStepApiController(ObjectMapper objectMapper, HttpServletRequest request) {
+        this.objectMapper = objectMapper;
+        this.request = request;
+    }
+
+    public ResponseEntity<?> getStep(@ApiParam(value = "request body get enigma step", required = true) @Valid @RequestBody JsonApiBodyRequest body) {
+        if (!isStepValid(body)) {
+            return new ResponseEntity<>(createResponseErrors(body), HttpStatus.BAD_REQUEST);
+        }
+        
+        return new ResponseEntity<>(createResponseSuccess(body), HttpStatus.OK);
+    }
+
+    private boolean isStepValid(JsonApiBodyRequest body) {
+        return body.getData().get(0).getStep().equalsIgnoreCase("1");
+    }
+    
+    @GetMapping("/get-One")
+    public ResponseEntity<String> getOne() {
+    	return new ResponseEntity<>("Step 1: Open the refrigerator", HttpStatus.OK);
+    }
+    
+    private List<JsonApiBodyResponseErrors> createResponseErrors(JsonApiBodyRequest body) {
+    	ErrorDetail errorDetail = new ErrorDetail();
+    	errorDetail.setCode("001");
+    	errorDetail.setDetail("Step: " + body.getData().get(0).getStep() + " not supported - Expected: 1");
+    	errorDetail.setId(body.getData().get(0).getHeader().getId());
+    	errorDetail.setSource("/getStep");
+    	errorDetail.setStatus("400");
+    	errorDetail.setTitle("Step not supported");
+    	
+    	JsonApiBodyResponseErrors responseError = new JsonApiBodyResponseErrors();
+    	responseError.addErrorsItem(errorDetail);
+    	
+    	List<JsonApiBodyResponseErrors> responseErrorsList = new ArrayList<JsonApiBodyResponseErrors>(); 
+    	responseErrorsList.add(responseError);
+    	
+    	return responseErrorsList;
+    }
+    
+    private List<JsonApiBodyResponseSuccess> createResponseSuccess(JsonApiBodyRequest body) {
+        GetEnigmaStepResponse responseEnigma = new GetEnigmaStepResponse();    
+        responseEnigma.setHeader(body.getData().get(0).getHeader());
+        responseEnigma.setStep(body.getData().get(0).getStep());
+        responseEnigma.setStepDescription("Open the refrigerator");
+        
+        JsonApiBodyResponseSuccess responseSuccess = new JsonApiBodyResponseSuccess();
+        responseSuccess.addDataItem(responseEnigma);
+        
+        return List.of(responseSuccess);
+    }
